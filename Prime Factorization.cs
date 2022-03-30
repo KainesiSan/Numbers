56414da440d3d5436b78b59dab94fdd8f7618d3d
KainesiSan <102213825+KainesiSan@users.noreply.github.com>
2022-03-30 19:50:56 +0500

    Add files via upload

---
+++ Prime Factorization.cs
@@ -0,0 +1,75 @@
+using System;
+
+namespace CSharp_Shell
+{
+    class PrimeFactorization
+    {
+        static void Main()
+        {
+            /* Число разложенное на множители */
+            double number;
+            
+            /* Делитель */
+            double divider;
+            
+            /* Предел делителя */
+            double limit;
+            
+            /* Вывод множителей */
+            string output = null;
+            
+            try
+            {
+                Console.Write("Введите число: ");
+                number = Convert.ToDouble(Console.ReadLine());
+                
+                divider = 2;
+                limit = Convert.ToDouble(Math.Sqrt(number) + 1);
+                
+                while (divider < limit)
+                {
+                    if (number % divider == 0) 
+                    {
+                        if (number / divider * divider - number == 0)
+                        {
+                            output += divider.ToString() + "*";
+                            number = number / divider;
+                            limit = Convert.ToDouble(Math.Sqrt(number) + 1);
+                        }
+                        else
+                        {
+                            divider += 1;
+                        }
+                    }
+                    else
+                    {
+                        divider += 1;
+                    }
+                }
+                
+                if (number == 0)
+                {
+                    output = "Число должно быть натуральным!";
+                }
+                else if (number == 1)
+                {
+                    output = number.ToString();
+                }
+                else if (output == number.ToString())
+                {
+                    output = $"Число {number} - простое.";
+                }
+                else if (output != number.ToString())
+                {
+                    output += number.ToString();
+                }
+                
+                Console.WriteLine(output);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Число не может содержать пустоту, буквы или спецсимволы.");
+            }
+        }
+    }
+}
